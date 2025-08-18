1493cf65b90289451e00bace71729b7bb887d7d9
Michael <michael.mikhalevich@gmail.com>
2025-08-18 15:51:23 +0300

    first backoff implementation (#1)

---
+++ example/consumer/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	rabbitmqbackoff "github.com/Mikhalevich/rabbitmq-backoff"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+const (
+	workersCount = 3
+)
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	panicOnError(err, "failed to dial rabbitmq")
+
+	defer conn.Close()
+
+	channel, err := conn.Channel()
+	panicOnError(err, "failed to open channel")
+
+	defer channel.Close()
+
+	err = channel.ExchangeDeclare(
+		"test_exchange",
+		"direct",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	panicOnError(err, "failed to declare exchange")
+
+	consumer, err := rabbitmqbackoff.NewConsumer(
+		channel,
+		rabbitmqbackoff.Queue{
+			Name: "test_queue",
+		},
+		[]int64{1000, 2000, 4000, 8000},
+	)
+
+	panicOnError(err, "failed to init consumer")
+
+	err = channel.QueueBind(
+		"test_queue",
+		"test_key",
+		"test_exchange",
+		false,
+		nil,
+	)
+
+	panicOnError(err, "failed to bind main queue")
+
+	err = consumer.Consume(
+		context.Background(), workersCount,
+		func(ctx context.Context, d amqp.Delivery) error {
+			log.Printf("received: %s time: %s", string(d.Body), time.Now().Format(time.TimeOnly))
+
+			return d.Nack(false, false)
+		},
+	)
+	panicOnError(err, "failed to consume messages")
+}
+
+func panicOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %v", msg, err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/Mikhalevich/rabbitmq-backoff
+
+go 1.24.6
+
+require (
+	github.com/rabbitmq/amqp091-go v1.10.0
+	golang.org/x/sync v0.16.0
+)
